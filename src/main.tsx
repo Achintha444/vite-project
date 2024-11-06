db6c88639dd9d30ed336ee229d13ca4309a61dc0
Achintha Isuru <achinthaisuru444@gmail.com>
2024-11-06 00:07:49 -0500

    small change

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Assignment from './SquareDisplayAssignment.tsx'
+import SquareDisplayAssignment from './SquareDisplayAssignment.tsx'
 import './index.css'
 
 const colorList = ["red", "green", "blue"];
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Assignment colorList={colorList} />
+    <SquareDisplayAssignment colorList={colorList} />
   </StrictMode>,
 )
